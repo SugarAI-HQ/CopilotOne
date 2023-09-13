d17a049f7d98acc0e1c7a72133418ea1fd61e4e6
Ankur Agarwal <signup.ankur@gmail.com>
2023-09-14 00:22:29 +0530

    fixes for env

--- src/env.mjs
+++ src/env.mjs
@@ -24,7 +24,7 @@ export const env = createEnv({
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
 
-    OPENAI_API_KEY: z.string(),
+    OPENAI_API_KEY: z.string().optional(),
   },
 
   /**
