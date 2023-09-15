51ef0f274ebb91f9d3b4b863761721acae2274d3
Vipul <vipulrpatil.8@gmail.com>
2023-09-15 18:32:30 +0530

    route guard updated

--- src/components/RouteGuard.tsx
+++ src/components/RouteGuard.tsx
@@ -3,6 +3,7 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import PageLoader from './PageLoader';
 
 export default function RouteGuard({ children }: { children: React.ReactNode }) {
   const { data: session } = useSession();
@@ -10,16 +11,20 @@ export default function RouteGuard({ children }: { children: React.ReactNode }) 
   const [loading, setLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
-    // Check if the session is undefined or null (loading or not authenticated)
  
     if (session === null) {
-    //
       router.push('/auth/login');
-    }else{
-         setLoading(false)
+    }
+    if(session) {
+      setLoading(false)
     }
   }, [session, router]);
 
-  // Render the children if authenticated, a loading indicator, or a message
-  return loading ? <p>Loading...</p> : session ? <>{children}</> : <p>Not authenticated.</p>;
+  if(loading){
+    return <PageLoader/>
+  }
+
+  if(session){
+    return <>{children}</>
+  }
 }
