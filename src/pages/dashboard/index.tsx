3c4312196f888f5a285c983addfce52878e521e6
Ankur Agarwal <signup.ankur@gmail.com>
2023-10-02 22:30:35 +0530

    fixes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -9,15 +9,15 @@ const Index = () => {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const handlePackageCreate = () => {
-    console.log
-  }
+    console.log;
+  };
 
   return (
     <div className="w-full">
       <Head>
         <title>Sugar Factor</title>
         <meta name="description" content="Build your prompts" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#040306] to-[#494952]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -25,7 +25,6 @@ const Index = () => {
             Sugar <span className="text-[hsl(280,100%,70%)]">Factory</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            
             <Link
               href="/dashboard/prompts"
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
@@ -53,8 +52,8 @@ const Index = () => {
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
-                Learn more about Sugarcane AI, architecture, problem it solves, and how
-                to deploy it.
+                Learn more about Sugarcane AI, architecture, problem it solves,
+                and how to deploy it.
               </div>
             </Link>
             <Link
