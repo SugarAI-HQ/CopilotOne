48898fe313a13c99d018fda4b6fa97f4823f1dfe
Ankur Agarwal <signup.ankur@gmail.com>
2023-11-10 17:03:28 +0530

    remving cloudflare (#59)

---
+++ apps/factory/src/components/landing/hero.tsx
@@ -0,0 +1,101 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+
+import Head from "next/head";
+import Link from "next/link";
+const Hero = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="w-full">
+      <Head>
+        <title>Sugarcane AI</title>
+        <meta
+          name="description"
+          content="Supercharge your cross LLM App development by decoupling your prompts from the Application Layer"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#040306] to-[#494952]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          {/* <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Sugarcane <span className="text-[hsl(280,100%,70%)]">AI</span>
+          </h1> */}
+          <Image
+            src="/images/logo-transparent.png"
+            width={600}
+            height={600}
+            alt="Sugarcane AI logo"
+          />
+
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              href={sessionData ? "/dashboard/prompts" : "/api/auth/signin"}
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            >
+              <h3 className="text-2xl font-bold">Sugar Factory →</h3>
+              <div className="text-lg">
+                Build, Version, Train and Ship Prompt Packages over APIs.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/marketplace/packages"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Sugar Hub →</h3>
+              <div className="text-lg">
+                Discover and Integrate Prompt Packages built by the community.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://sugarcaneai.dev/docs"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Documentation →</h3>
+              {/* <div className="text-lg">
+                Learn more about Sugarcane AI, architecture, problem it solves,
+                and how to deploy it.
+              </div> */}
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://github.com/sugarcane-ai"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Contribute →</h3>
+              {/* <div className="text-lg">
+                Checkout the code and make your first contribution.
+              </div> */}
+            </Link>
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <AuthShowcase />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.username}</span>}
+      </p>
+      <button
+        className="text-xlg rounded-full bg-white/10 px-10 py-6 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
+
+export default Hero;
--- apps/factory/src/pages/index.tsx
+++ apps/factory/src/pages/index.tsx
@@ -1,53 +1,7 @@
-// pages/index.tsx
-
-import React, { useEffect, useState } from "react";
-import Head from "next/head";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import NextAuth from "next-auth";
-
-import { authOptions } from "~/server/auth";
-
-// import { authOptions } from "~/server/auth";
-
-// export default NextAuth(authOptions);
-
-import { signIn, useSession } from "next-auth/react";
-
-export function Login() {
-  return (
-    <Button color="inherit" variant="outlined" onClick={() => void signIn()}>
-      Login
-    </Button>
-  );
-}
+import Hero from "~/components/landing/hero";
 
 function Index() {
-  const { data: session } = useSession();
-  return (
-    <div>
-      <Head>
-        <title>Sugarcane AI</title>
-      </Head>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Sugarcane AI
-          </Typography>
-          <Login />
-        </Toolbar>
-      </AppBar>
-      <Container sx={{ marginTop: 2 }}>
-        <Typography variant="h4" gutterBottom>
-          Get Started
-        </Typography>
-        <Login />
-      </Container>
-    </div>
-  );
+  return <Hero></Hero>;
 }
 
 export default Index;
