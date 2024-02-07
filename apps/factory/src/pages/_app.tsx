6f0d82aa074dc55b06e2937b729e9b9a4da9892f
Lovedeep <91473234+LovedeepSingh19@users.noreply.github.com>
2024-02-08 01:58:17 +0530

    Google analytics  (#188)
    * Google analytics - added
    
    * env file updates

--- apps/factory/src/pages/_app.tsx
+++ apps/factory/src/pages/_app.tsx
@@ -5,7 +5,8 @@ import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
+import { GoogleAnalytics, event } from "nextjs-google-analytics";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,6 +16,19 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(name, {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 const MyApp = ({
   Component,
   pageProps: { session, ...pageProps },
@@ -22,6 +36,11 @@ const MyApp = ({
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <SessionProvider session={session as Session}>
+      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+        <GoogleAnalytics
+          gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}
+        />
+      )}
       {getLayout(<Component {...pageProps} />)}
       <Toaster position="top-right" />
     </SessionProvider>
