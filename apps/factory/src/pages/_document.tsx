f751ff907f7a95374fba853259eed4af5575601e
Pulkit Agarwal <pulkit.ag02@gmail.com>
2024-03-04 18:09:38 +0530

    change the script tag

--- apps/factory/src/pages/_document.tsx
+++ apps/factory/src/pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script src="/__ENV.js" />
+        <script src="/__ENV.js" />
       </Head>
       <body>
         <Main />
