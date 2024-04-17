2b9badd285b7da5b195ae09802f861b4803bf754
Ankur Agarwal <signup.ankur@gmail.com>
2024-04-17 22:05:04 +0530

    releaseing sdk 0.0.9

--- sdks/js/src/react/CopilotContext.tsx
+++ sdks/js/src/react/CopilotContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 import {
   type SkillRegistrationType,
   type CopilotConfigType,
@@ -210,5 +210,5 @@ export const CopilotProvider = function ({
 };
 
 export function useCopilot() {
-  return React.useContext(CopilotContext);
+  return useContext(CopilotContext);
 }
