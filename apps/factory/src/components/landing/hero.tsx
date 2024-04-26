6982f7dfacc0a77599291443e44846ee44133fe5
Ankur Agarwal <signup.ankur@gmail.com>
2024-04-26 22:18:45 +0530

    fixes for non loggedin user

--- apps/factory/src/components/landing/hero.tsx
+++ apps/factory/src/components/landing/hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-md bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/dashboard/copilots"
+              href={sessionData ? "/dashboard/copilots" : "/api/auth/signin"}
             >
               <h3 className="text-2xl font-bold">Build Copilot →</h3>
               <div className="text-lg">
